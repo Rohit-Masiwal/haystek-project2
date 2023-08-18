3b66ec0b5cdf02d472b15386cc8bd283487aee24
Rohit Masiwal <rohitmasiwal2@gmail.com>
2023-08-19 01:28:19 +0530

    Commiting Changes

---
+++ index.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>People Data</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="media-queries.css">
+</head>
+
+<body class="container">
+<div class="app-box">
+    <div class="app-heading-box">
+        <p class="p1">PEOPLE DATA</p>
+        <button type="button" class="button">NEXT PERSON</button>
+    </div>
+    <div class="app-data-table">
+        <table class="customers">
+            <tr>
+                <th rowspan="2" style="width:7%">1
+                </th>
+                <td>Name: John Smith</td>
+            </tr>
+            <tr>
+                <td>Location: New York, NY</td>
+            </tr>
+        </table>
+        <table class="customers">
+            <tr>
+                <th rowspan="2" style="width:7%">2
+                </th>
+                <td>Name: Jane Doe</td>
+            </tr>
+            <tr>
+                <td>Location: Los Angeles, CA</td>
+            </tr>
+        </table>
+        <table class="customers">
+            <tr>
+                <th rowspan="2" style="width:7%">3
+                </th>
+                <td>Name: Bob Johnson</td>
+            </tr>
+            <tr>
+                <td>Location: Chicago, IL</td>
+            </tr>
+        </table>
+    </div>
+</div>
+</body>
+
+</html>
